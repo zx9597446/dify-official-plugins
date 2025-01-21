fca9ced7ac4aa443d78ef1eb652a25039d41316e
Yeuoly <45712896+Yeuoly@users.noreply.github.com>
2025-01-21 21:22:40 +0800

    bump: gemini to 0.0.2 (#156)

--- models/gemini/main.py
+++ models/gemini/main.py
@@ -3,7 +3,7 @@ from dify_plugin import Plugin, DifyPluginEnv
 
 grpc.experimental.gevent.init_gevent()
 
-plugin = Plugin(DifyPluginEnv(MAX_REQUEST_TIMEOUT=120))
+plugin = Plugin(DifyPluginEnv())
 
 if __name__ == '__main__':
     plugin.run()
