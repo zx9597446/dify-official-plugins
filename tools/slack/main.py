c041fd3ee737ffedf641bfddaec8370ecef624c8
Yeuoly <45712896+Yeuoly@users.noreply.github.com>
2025-01-21 21:20:56 +0800

    bump: slack to 0.0.2 (#161)

--- tools/slack/main.py
+++ tools/slack/main.py
@@ -1,6 +1,6 @@
 from dify_plugin import Plugin, DifyPluginEnv
 
-plugin = Plugin(DifyPluginEnv(MAX_REQUEST_TIMEOUT=120))
+plugin = Plugin(DifyPluginEnv())
 
 if __name__ == '__main__':
     plugin.run()
