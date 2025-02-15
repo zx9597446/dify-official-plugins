e786da57a010483b0056cf414b0279889eba0149
Đỗ Quang Minh(Martin) <martin@techainer.com>
2025-02-15 11:39:12 +0700

    fix comfyui for wss connection (#214)

--- tools/comfyui/provider/comfyui.py
+++ tools/comfyui/provider/comfyui.py
@@ -9,7 +9,10 @@ class ComfyUIProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         ws = websocket.WebSocket()
         base_url = URL(credentials.get("base_url"))
-        ws_address = f"ws://{base_url.authority}/ws?clientId=test123"
+        ws_protocol = "ws"
+        if base_url.scheme == "https":
+            ws_protocol = "wss"
+        ws_address = f"{ws_protocol}://{base_url.authority}/ws?clientId=test123"
         try:
             ws.connect(ws_address)
         except Exception:
