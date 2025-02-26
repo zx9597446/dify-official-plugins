fff12d5dbefbb215db8346cb0587f916b59180be
Yeuoly <45712896+Yeuoly@users.noreply.github.com>
2025-02-26 18:40:21 +0800

    Plugin/feishu (#272)
    * plugin: feishu
    
    * plugin: feishu
    
    * plugin: feishu

--- tools/feishu/tools/feishu_group_bot.py
+++ tools/feishu/tools/feishu_group_bot.py
@@ -1,14 +1,23 @@
-from typing import Any, Union
+from collections.abc import Generator
+from typing import Any
+import uuid
 import httpx
 from dify_plugin.entities.tool import ToolInvokeMessage
 from dify_plugin import Tool
-from core.tools.utils.uuid_utils import is_valid_uuid
+
+
+def is_valid_uuid(uuid_str: str) -> bool:
+    try:
+        uuid.UUID(uuid_str)
+        return True
+    except ValueError:
+        return False
 
 
 class FeishuGroupBotTool(Tool):
     def _invoke(
-        self, user_id: str, tool_parameters: dict[str, Any]
-    ) -> Union[ToolInvokeMessage, list[ToolInvokeMessage]]:
+        self, tool_parameters: dict[str, Any]
+    ) -> Generator[ToolInvokeMessage, None, None]:
         """
         invoke tools
         API document: https://open.feishu.cn/document/client-docs/bot-v3/add-custom-bot
@@ -16,10 +25,14 @@ class FeishuGroupBotTool(Tool):
         url = "https://open.feishu.cn/open-apis/bot/v2/hook"
         content = tool_parameters.get("content", "")
         if not content:
-            return self.create_text_message("Invalid parameter content")
+            yield self.create_text_message("Invalid parameter content")
+            return
         hook_key = tool_parameters.get("hook_key", "")
         if not is_valid_uuid(hook_key):
-            return self.create_text_message(f"Invalid parameter hook_key ${hook_key}, not a valid UUID")
+            yield self.create_text_message(
+                f"Invalid parameter hook_key ${hook_key}, not a valid UUID"
+            )
+            return
         msg_type = "text"
         api_url = f"{url}/{hook_key}"
         headers = {"Content-Type": "application/json"}
@@ -28,10 +41,12 @@ class FeishuGroupBotTool(Tool):
         try:
             res = httpx.post(api_url, headers=headers, params=params, json=payload)
             if res.is_success:
-                return self.create_text_message("Text message sent successfully")
+                yield self.create_text_message("Text message sent successfully")
             else:
-                return self.create_text_message(
+                yield self.create_text_message(
                     f"Failed to send the text message, status code: {res.status_code}, response: {res.text}"
                 )
         except Exception as e:
-            return self.create_text_message("Failed to send message to group chat bot. {}".format(e))
+            yield self.create_text_message(
+                "Failed to send message to group chat bot. {}".format(e)
+            )
