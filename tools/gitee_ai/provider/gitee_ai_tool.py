43e7d796a0e392598534bee16d357dea09e11300
方程 <1787003204@qq.com>
2025-02-28 18:31:45 +0800

    Gitee AI Tool 更新 (#333)
    Co-authored-by: 方程 <fangcheng@oschina.cn>

--- tools/gitee_ai/provider/gitee_ai_tool.py
+++ tools/gitee_ai/provider/gitee_ai_tool.py
@@ -3,7 +3,7 @@ from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 from dify_plugin import ToolProvider
 
 
-class GiteeAIProvider(ToolProvider):
+class GiteeAIToolProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict) -> None:
         url = "https://ai.gitee.com/api/base/account/me"
         headers = {"accept": "application/json", "authorization": f"Bearer {credentials.get('api_key')}"}
