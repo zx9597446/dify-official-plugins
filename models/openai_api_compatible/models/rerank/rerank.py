8670f73bea48a317d63a1e7ac2db80054b0fea95
jcvv0n <56829623+jcvv0n@users.noreply.github.com>
2025-03-03 14:27:08 +0800

    Fix failed to create rerank model when use vllm rerank (#362)
    * Fix failed to create rerank model when use vllm rerank
    
    * Update plugin version

--- models/openai_api_compatible/models/rerank/rerank.py
+++ models/openai_api_compatible/models/rerank/rerank.py
@@ -4,4 +4,27 @@ from dify_plugin.interfaces.model.openai_compatible.rerank import (
 
 
 class OpenAIRerankModel(OAICompatRerankModel):
-    pass
+    def validate_credentials(self, model: str, credentials: dict) -> None:
+        """
+        Validate model credentials
+
+        :param model: model name
+        :param credentials: model credentials
+        :return:
+        """
+        try:
+            self._invoke(
+                model=model,
+                credentials=credentials,
+                query="What is the capital of the United States?",
+                docs=[
+                    "Carson City is the capital city of the American state of Nevada. At the 2010 United States "
+                    "Census, Carson City had a population of 55,274.",
+                    "The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that "
+                    "are a political division controlled by the United States. Its capital is Saipan.",
+                ],
+                score_threshold=0.8,
+                top_n=3,
+            )
+        except Exception as ex:
+            raise CredentialsValidateFailedError(str(ex)) from ex
