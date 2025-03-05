66cdcadb333f6cbbb5d7173c7535b684949ee981
zxfishhack <zxfishhack@gmail.com>
2025-03-05 12:50:04 +0800

    add agent though config to OpenAI-API-compatible (#381)

--- models/openai_api_compatible/models/llm/llm.py
+++ models/openai_api_compatible/models/llm/llm.py
@@ -1,7 +1,20 @@
+from typing import Mapping
+
+from dify_plugin.entities.model import AIModelEntity, ModelFeature
 from dify_plugin.interfaces.model.openai_compatible.llm import (
     OAICompatLargeLanguageModel,
 )
 
 
 class OpenAILargeLanguageModel(OAICompatLargeLanguageModel):
-    pass
+    def get_customizable_model_schema(self, model: str, credentials: Mapping) -> AIModelEntity:
+        entity = super().get_customizable_model_schema(model, credentials)
+
+        agent_though_support = credentials.get("agent_though_support", "not_supported")
+        if agent_though_support == "supported":
+            try:
+                entity.features.index(ModelFeature.AGENT_THOUGHT)
+            except ValueError:
+                entity.features.append(ModelFeature.AGENT_THOUGHT)
+
+        return entity
